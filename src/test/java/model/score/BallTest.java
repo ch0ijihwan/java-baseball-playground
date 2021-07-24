6654db7d505c0883c5047a9d047f3e1f9a2880c8
cjh8746 <cjh87467@gmail.com>
2021-07-24 23:41:51 +0900

    style :  불필요한 줄 띄움 제거

--- src/test/java/model/score/BallTest.java
+++ src/test/java/model/score/BallTest.java
@@ -13,36 +13,27 @@ class BallTest {
     @Test
     @DisplayName("(두 리스트 값이 다 같은 경우) 두개의 리스트를 받아, 각 리스트에 같은 값이 몇개인지 반환")
     public void guess() {
-
-
         List<String> input = Arrays.asList("1", "2", "3");
         List<String> random = Arrays.asList("1", "2", "3");
         Ball ball = new Ball(input, random);
         assertThat(ball.countBall()).isEqualTo(3);
     }
 
-
     @Test
     @DisplayName("(두 리스트 중 하나의 요소 값만 다른 경우) 두개의 리스트를 받아, 각 리스트에서 볼 값을 반환")
     public void guess_1() {
-
-
         List<String> input = Arrays.asList("1", "8", "3");
         List<String> random = Arrays.asList("1", "2", "3");
         Ball ball = new Ball(input, random);
         assertThat(ball.countBall()).isEqualTo(2);
     }
 
-
     @Test
     @DisplayName("(두 리스트 값이 모두 다른 경우) 두개의 리스트를 받아, 각 리스트에서 볼 값을 반환")
     public void guess_2() {
-
-
         List<String> input = Arrays.asList("1", "2", "3");
         List<String> random = Arrays.asList("4", "5", "6");
         Ball ball = new Ball(input, random);
         assertThat(ball.countBall()).isEqualTo(0);
     }
-
 }
