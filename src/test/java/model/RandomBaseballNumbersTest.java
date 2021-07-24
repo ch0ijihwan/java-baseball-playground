14d8e34497ab678c264cd76d05bc01eedb1931c8
cjh8746 <cjh87467@gmail.com>
2021-07-24 23:42:48 +0900

    test : 랜덤 수 3 개를 반환하는 메소드의 테스트코드 구현

--- src/test/java/model/RandomBaseballNumbersTest.java
+++ src/test/java/model/RandomBaseballNumbersTest.java
@@ -4,25 +4,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MakeRandomBaseballTest {
-
-    private MakeRandomBaseball makeRandomBaseball;
-    private Set<String> input;
+class RandomBaseballNumbersTest {
+    private RandomBaseballNumbers makeRandomBaseball;
 
     @BeforeEach
     void setUp() {
-        makeRandomBaseball = new MakeRandomBaseball();
-        Set<String> input = new HashSet<>();
+        makeRandomBaseball = new RandomBaseballNumbers();
     }
 
     @Test
     @DisplayName("숫자 야구 게임을 위한 무작위 난수 생성. 3자리 수 숫자 반환")
     public void makeNumber() {
-        assertThat(makeRandomBaseball.outRandomBaseball(input).size()).isEqualTo(3);
+        assertThat(makeRandomBaseball.outRandomBaseball().size()).isEqualTo(3);
     }
 }
