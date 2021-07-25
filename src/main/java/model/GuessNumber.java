e2820c920a6ed35830999420218ef5c08e6107e2
cjh8746 <cjh87467@gmail.com>
2021-07-25 17:09:56 +0900

    feat : 받은 문자열 리스트와 과 랜덤으로 생성된 리스트를 비교하여 볼, 스트라이크를 출력해주는 기능 구현

---
+++ src/main/java/model/GuessNumber.java
@@ -0,0 +1,35 @@
+package model;
+
+import model.score.Ball;
+import model.score.Strike;
+import view.Input;
+
+import java.util.List;
+
+public class GuessNumber {
+    private List<String> inputNumbers;
+    private List<String> randomNumbers;
+    private Strike strike;
+    private Ball ball;
+
+    public GuessNumber(List<String> inputNumbers, List<String> randomNumbers) {
+        this.inputNumbers = inputNumbers;
+        this.randomNumbers = randomNumbers;
+    }
+
+    public void guessResult() {
+        int strikeCount = 0;
+
+        while (true) {
+            Strike strike = new Strike(this.inputNumbers, this.randomNumbers);
+            Ball ball = new Ball(this.inputNumbers, this.randomNumbers);
+            if(strikeCount==3){
+                break;
+            }
+            System.out.println("볼: " + ball.countBall() + " 스트라이크 : " + strike.countStrike());
+            CheckInput checkInput = new CheckInput(Input.InputBaseballNumber());
+            this.inputNumbers = checkInput.checkInputNumber();
+            strikeCount = strike.countStrike();
+        }
+    }
+}
