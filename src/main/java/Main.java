00073684bc2f50bf8707501bb5286b40532e8bf3
cjh8746 <cjh87467@gmail.com>
2021-07-25 17:10:58 +0900

    feat : 야구게임 시작을 위한 Main 클래스 구현

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import controller.BaseballController;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        BaseballController baseballController = new BaseballController();
+        baseballController.run();
+
+    }
+}
