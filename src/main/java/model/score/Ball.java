f6f87cca88a541ca968f27117ba3e964c6033c05
cjh8746 <cjh87467@gmail.com>
2021-07-25 16:46:22 +0900

    feat : Ball 의 개수를 반환해주는 countBall 구현

---
+++ src/main/java/model/score/Ball.java
@@ -0,0 +1,25 @@
+package model.score;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Ball {
+    List<String> inputNumbers;
+    List<String> randomNumbers;
+    Strike strike;
+
+    public Ball(List<String> inputNumbers, List<String> randomNumbers) {
+        this.inputNumbers = inputNumbers;
+        this.randomNumbers = randomNumbers;
+        strike = new Strike(this.inputNumbers,this.randomNumbers);
+    }
+
+    public int countBall() {
+        return (int) randomNumbers.stream()
+                .filter(ball -> inputNumbers.stream()
+                        .anyMatch(Predicate
+                                .isEqual(ball))).count() - strike.countStrike();
+
+    }
+}
+
