a7f4c1ee0f161181bf28319279527eb3a4467f20
cjh8746 <cjh87467@gmail.com>
2021-08-03 00:58:08 +0900

    style : 불 필요한 줄 띄움 제거

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
 import controller.BaseballController;
 
 public class Main {
-
-
-
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         BaseballController baseballController = new BaseballController();
         baseballController.run();
-
     }
 }
