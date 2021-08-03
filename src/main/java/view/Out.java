9e9073db25ed8a801586b7f81fd10dad7d70f4fe
cjh8746 <cjh87467@gmail.com>
2021-08-03 14:49:24 +0900

    refactor : 3 스트라이크시 컨트롤러로 부터 값을 받는 threeStrike 메소드 생성

--- src/main/java/view/Out.java
+++ src/main/java/view/Out.java
@@ -1,7 +1,11 @@
 package view;
 
 public class Out {
-     public static void resultOut(int ball, int strike){
+    public static void resultOut(int ball, int strike) {
         System.out.println("볼: " + ball + " 스트라이크 : " + strike);
+    }
+
+    public static void threeStrike() {
+        System.out.println("3 스트라이크 입니다.");
     }
 }
