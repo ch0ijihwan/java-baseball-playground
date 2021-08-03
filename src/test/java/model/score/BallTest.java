8d1a27faafd8c90be2cbd406f60c4c5eaa7baf5e
cjh8746 <cjh87467@gmail.com>
2021-08-03 14:43:08 +0900

    test : 두 리스트를 받아 볼의 개수를 반환하는 테스트 코드 생성

--- src/test/java/model/score/BallTest.java
+++ src/test/java/model/score/BallTest.java
@@ -1,39 +1,37 @@
 package model.score;
 
+import model.Number;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class BallTest {
-
-    @Test
-    @DisplayName("(두 리스트 값이 다 같은 경우) 두개의 리스트를 받아, 각 리스트에 같은 값이 몇개인지 반환")
-    public void guess() {
-        List<String> input = Arrays.asList("1", "2", "3");
-        List<String> random = Arrays.asList("1", "2", "3");
-        Ball ball = new Ball(input, random);
-        assertThat(ball.countBall()).isEqualTo(3);
+    @ParameterizedTest
+    @MethodSource("ballCountParameterProvider")
+    @DisplayName("4가지 두 Set을 받아, 볼의 개수를 반환한다.")
+    void getBallCount(Set<Number> inputNumber, Set<Number> randomNumber, int expect) {
+        int count = Ball.countBall(inputNumber, randomNumber);
+        assertThat(count).isEqualTo(expect);
     }
 
-    @Test
-    @DisplayName("(두 리스트 중 하나의 요소 값만 다른 경우) 두개의 리스트를 받아, 각 리스트에서 볼 값을 반환")
-    public void guess_1() {
-        List<String> input = Arrays.asList("1", "8", "3");
-        List<String> random = Arrays.asList("1", "2", "3");
-        Ball ball = new Ball(input, random);
-        assertThat(ball.countBall()).isEqualTo(2);
-    }
-
-    @Test
-    @DisplayName("(두 리스트 값이 모두 다른 경우) 두개의 리스트를 받아, 각 리스트에서 볼 값을 반환")
-    public void guess_2() {
-        List<String> input = Arrays.asList("1", "2", "3");
-        List<String> random = Arrays.asList("4", "5", "6");
-        Ball ball = new Ball(input, random);
-        assertThat(ball.countBall()).isEqualTo(0);
+    static Stream<Arguments> ballCountParameterProvider() {
+        return Stream.of(arguments(new LinkedHashSet<>(Arrays.asList(new Number("1"), new Number("2"), new Number("3"))),
+                        new LinkedHashSet<>(Arrays.asList(new Number("3"), new Number("1"), new Number("2"))), 3),
+                arguments(new LinkedHashSet<>(Arrays.asList(new Number("1"), new Number("2"), new Number("3"))),
+                        new LinkedHashSet<>(Arrays.asList(new Number("2"), new Number("3"), new Number("4"))), 2),
+                arguments(new LinkedHashSet<>(Arrays.asList(new Number("1"), new Number("2"), new Number("3"))),
+                        new LinkedHashSet<>(Arrays.asList(new Number("2"), new Number("5"), new Number("7"))), 1),
+                arguments(new LinkedHashSet<>(Arrays.asList(new Number("1"), new Number("2"), new Number("3"))),
+                        new LinkedHashSet<>(Arrays.asList(new Number("4"), new Number("5"), new Number("6"))), 0)
+        );
     }
 }
