f08c987c8235a6cf9d3f9e6922af36b2dd195f89
cjh8746 <cjh87467@gmail.com>
2021-08-03 14:48:39 +0900

    test : 입력 받은 숫자야구 값의 유효성을 검사하는 테스트 코드 추가

---
+++ src/test/java/model/InputNumbersTest.java
@@ -0,0 +1,19 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class InputNumbersTest {
+    private InputNumbers inputNumbers;
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 1 1", "1 1 2", "1 2 1", "3 3 3"})
+    @DisplayName("세가지의 수를 받고, 중복되는 수가 있을 경우 예외처리 반환")
+    public void DuplicateCheckTest(String values) {
+        inputNumbers = new InputNumbers(values);
+        assertThrows(IllegalArgumentException.class, () -> inputNumbers.checkInputNumber());
+    }
+}
