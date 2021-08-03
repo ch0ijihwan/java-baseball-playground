c90a84d3eb5fd254eddd24f934e75844e34c759c
cjh8746 <cjh87467@gmail.com>
2021-08-03 17:31:14 +0900

    refactor: Main부 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
 import controller.BaseballController;
 
+import static view.Input.AskNewGame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        BaseballController baseballController = new BaseballController();
-        baseballController.run();
+        boolean newGame = true;
+        while (newGame) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            BaseballController baseballController = new BaseballController();
+            baseballController.run();
+            newGame = AskNewGame();
+        }
+
+
     }
 }
