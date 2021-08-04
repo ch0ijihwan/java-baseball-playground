4479601370e6126d75d2656d4fe7307d00b5f0a0
cjh8746 <cjh87467@gmail.com>
2021-08-04 15:12:26 +0900

    refactor : 3 스트라이크 출력을 view로 분리

--- src/main/java/model/BaseBallScore.java
+++ src/main/java/model/BaseBallScore.java
@@ -3,15 +3,13 @@ package model;
 import controller.BaseballController;
 import model.score.Ball;
 import model.score.Strike;
-import view.Input;
 
-public class GuessNumber {
+public class BaseBallScore {
     private InputNumbers inputNumbers;
     private final RandomBaseballNumbers randomNumbers;
     private final BaseballController baseballController;
 
-
-    public GuessNumber(InputNumbers inputNumbers, RandomBaseballNumbers randomBaseballNumbers) {
+    public BaseBallScore(InputNumbers inputNumbers, RandomBaseballNumbers randomBaseballNumbers) {
         this.inputNumbers = inputNumbers;
         this.randomNumbers = randomBaseballNumbers;
         baseballController = new BaseballController();
@@ -21,20 +19,16 @@ public class GuessNumber {
         int strikeCount = 0;
 
         while (!isThreeStrike(strikeCount)) {
-            baseballController.guessResult(Ball.countBall(inputNumbers.getInputNumbers(), randomNumbers.getRandomBaseballNumbers()
+            baseballController.showResult(Ball.countBall(inputNumbers.getInputNumbers(), randomNumbers.getRandomBaseballNumbers()
             ), Strike.countStrike(inputNumbers.getInputNumbers(), randomNumbers.getRandomBaseballNumbers()));
 
-            this.inputNumbers = new InputNumbers(Input.InputBaseballNumber());
+            inputNumbers = baseballController.newBaseBallInput();
             strikeCount = Strike.countStrike(inputNumbers.getInputNumbers(), randomNumbers.getRandomBaseballNumbers());
+
         }
         baseballController.threeStrike();
     }
-
     boolean isThreeStrike(int strikeCount) {
-        if (strikeCount == 3) {
-            System.out.println("3 스트라이크!");
-            return true;
-        }
-        return false;
+        return strikeCount == 3;
     }
 }
