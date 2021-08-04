73430f6cf486c916027a420ae445d0dc4d2ef54e
cjh8746 <cjh87467@gmail.com>
2021-08-04 13:49:31 +0900

    refactor : 게임 시작 메시지 생성 및 게임 재시작 메시지를 받는 메소드 추가

--- src/main/java/controller/BaseballController.java
+++ src/main/java/controller/BaseballController.java
@@ -5,7 +5,9 @@ import model.InputNumbers;
 import model.RandomBaseballNumbers;
 import view.Out;
 
+import static view.Input.AskNewGame;
 import static view.Input.InputBaseballNumber;
+import static view.Out.showGameStart;
 
 public class BaseballController {
     private InputNumbers checkInput;
@@ -13,10 +15,15 @@ public class BaseballController {
     private GuessNumber guessNumber;
 
     public void run() {
-        checkInput = new InputNumbers(InputBaseballNumber());
-        randomBaseballNumbers = new RandomBaseballNumbers();
-        guessNumber = new GuessNumber(checkInput, randomBaseballNumbers);
-        guessNumber.guess();
+        boolean newGame = true;
+        while (newGame) {
+            showGameStart();
+            checkInput = new InputNumbers(InputBaseballNumber());
+            randomBaseballNumbers = new RandomBaseballNumbers();
+            guessNumber = new GuessNumber(checkInput, randomBaseballNumbers);
+            guessNumber.guess();
+            newGame = AskNewGame();
+        }
     }
 
     public void guessResult(int ball, int strike) {
