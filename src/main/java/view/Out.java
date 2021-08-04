cd35830fc83358417903252a01a4423521516167
cjh8746 <cjh87467@gmail.com>
2021-08-04 13:49:56 +0900

    refactor : 게임 시작 메시지 출력 메서드 추가

--- src/main/java/view/Out.java
+++ src/main/java/view/Out.java
@@ -8,4 +8,7 @@ public class Out {
     public static void threeStrike() {
         System.out.println("3 스트라이크 입니다.");
     }
+    public static void showGameStart(){
+        System.out.println("숫자야구 게임을 시작합니다.");
+    }
 }
