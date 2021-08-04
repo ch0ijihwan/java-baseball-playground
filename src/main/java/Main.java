3b972e0e2884403e91dafb02e80480bff9d718c5
cjh8746 <cjh87467@gmail.com>
2021-08-04 13:48:22 +0900

    refactor : 게임 시작 메시지 view에 넣어줌

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,8 @@
 import controller.BaseballController;
 
-import static view.Input.AskNewGame;
-
 public class Main {
     public static void main(String[] args) {
-        boolean newGame = true;
-        while (newGame) {
-            System.out.println("숫자 야구 게임을 시작합니다.");
-            BaseballController baseballController = new BaseballController();
-            baseballController.run();
-            newGame = AskNewGame();
-        }
-
-
+        BaseballController baseballController = new BaseballController();
+        baseballController.run();
     }
 }
