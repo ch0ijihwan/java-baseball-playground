572ede14f833a48496149d42e8a62e787bbaa0ed
cjh8746 <cjh87467@gmail.com>
2021-08-16 20:43:33 +0900

    refactor : 메소드 명 오류 수정

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -8,9 +8,8 @@ public class Input {
     {
         return SCANNER.next();
     }
-    public static boolean AskNewGame(){
+    public static boolean askNewGame(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return SCANNER.nextInt() == 1;
     }
-
 }
